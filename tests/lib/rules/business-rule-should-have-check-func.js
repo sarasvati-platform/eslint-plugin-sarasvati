158f613889fc2cdb67f02537e02ffa4a2075b510
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2021-11-24 11:39:10 +0300

    feat: business-rule-should-have-check-func (#1)

---
+++ tests/lib/rules/business-rule-should-have-check-func.js
@@ -0,0 +1,25 @@
+const rule = require('../../../lib/rules/business-rule-should-have-check-func')
+const RuleTester = require('eslint').RuleTester
+
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    }
+})
+
+ruleTester.run('business-rule-should-have-check-func', rule, {
+    valid: [
+        'class TestRule { check() {} }'
+    ],
+
+    invalid: [
+        {
+            code: 'class TestRule {}',
+            errors: [{
+                message: 'Business rule TestRule must have check method',
+                type: 'Identifier'
+            }],
+        },
+    ],
+})
